24942325d9d56dd1ac7514343cb0619fdab7a80f
CotivitiAdmin <jareena.shaik@cotiviti.com>
2018-02-16 11:59:02 +0000

    commiyyy

--- Testing/src/main/java/testpkg/MyTest.java
+++ Testing/src/main/java/testpkg/MyTest.java
@@ -4,7 +4,7 @@ public class MyTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing project started");
+		System.out.println("Testing project started ");
 
 	}
 
