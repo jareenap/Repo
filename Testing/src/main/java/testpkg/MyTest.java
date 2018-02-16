55b7ac6fffcb0b2e420b3f3a68fcdbd0273c437e
CotivitiAdmin <jareena.shaik@cotiviti.com>
2018-02-16 11:53:57 +0000

    committing from eclipse

---
+++ Testing/src/main/java/testpkg/MyTest.java
@@ -0,0 +1,11 @@
+package testpkg;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing project started");
+
+	}
+
+}
