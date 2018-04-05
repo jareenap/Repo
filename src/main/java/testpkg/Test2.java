4c92b9ebd7f97df62821dd373da0abe97dd3580d
jareenap <35337016+jareenap@users.noreply.github.com>
2018-04-05 16:02:02 +0000

    moved files

---
+++ src/main/java/testpkg/Test2.java
@@ -0,0 +1,10 @@
+package testpkg;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("test2 project started 111111");
+	}
+
+}
