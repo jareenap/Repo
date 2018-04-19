73d62101c4d2e32d1b2c2ef4bc2587ae67fd0817
jareenap <35337016+jareenap@users.noreply.github.com>
2018-04-19 13:38:50 +0000

    jenkins job 2

--- Testing/src/main/java/testpkg/MyTest.java
+++ Testing/src/main/java/testpkg/MyTest.java
@@ -5,7 +5,7 @@ public class MyTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing project started again12312312 ");
-
+System.out.println("Testing project started 1111 ");
 	}
 
 }
